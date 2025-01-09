2ecb8a4298296951797271d0d536241947a7cfb7
nickvourd <nickvourd97@gmail.com>
2025-01-09 15:00:20 -0600

    Update Utils.go

--- Packages/Utils/Utils.go
+++ Packages/Utils/Utils.go
@@ -1,8 +1,10 @@
 package Utils
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strconv"
 	"strings"
@@ -17,4 +19,33 @@ func CheckGoVersion() {
 		logger := log.New(os.Stderr, "[!] ", 0)
 		logger.Fatal("The version of Go is to old, please update to version 1.19.1 or later...\n")
 	}
+}
+
+// GetAbsolutePath function
+func GetAbsolutePath(filename string) (string, error) {
+	// Get the absolute path of the file
+	absolutePath, err := filepath.Abs(filename)
+	if err != nil {
+		return "", err
+	}
+	return absolutePath, nil
+}
+
+// GetFileSize function
+func GetFileSize(filePath string) (int64, error) {
+	// Open the file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return 0, fmt.Errorf("failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	// Get file information
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get file info: %v", err)
+	}
+
+	// Return the file size
+	return fileInfo.Size(), nil
 }
