d6f4ca21c15588d6121e77840064dc0dddfdacc9
Nikos Vourdas <32869078+nickvourd@users.noreply.github.com>
2025-03-09 19:13:47 -0700

    Add Word strategy

--- Packages/Reduce/Reduce.go
+++ Packages/Reduce/Reduce.go
@@ -1,12 +1,36 @@
 package Reduce
 
+import (
+	"SugarFree/Packages/WordList"
+	"log"
+	"strings"
+)
+
 // ApplyStrategy function
-func ApplyStrategy(binaryData []byte, numZeroBytes int) []byte {
-	zeroBytes := make([]byte, numZeroBytes)
+func ApplyStrategy(binaryData []byte, number int, strategy string) []byte {
+	switch strings.ToLower(strategy) {
+	case "zero":
+		zeroBytes := make([]byte, number)
+		//fmt.Println(zeroBytes)
 
-	result := make([]byte, len(binaryData)+numZeroBytes)
-	copy(result, binaryData)
-	copy(result[len(binaryData):], zeroBytes)
+		result := make([]byte, len(binaryData)+number)
+		copy(result, binaryData)
+		copy(result[len(binaryData):], zeroBytes)
 
-	return result
+		return result
+	case "word":
+		// Call function named SelectWords
+		words := WordList.SelectWords(number)
+		//fmt.Print(words)
+
+		wordsBytes := []byte(strings.Join(words, ""))
+		//fmt.Print(wordsBytes)
+
+		result := append(binaryData, wordsBytes...)
+
+		return result
+	default:
+		log.Fatal("Error: Invalid strategy provided. Please provide a valid strategy to continue...\n\n")
+		return nil
+	}
 }
